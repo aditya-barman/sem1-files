00ea475274f14395fde673acee2f8ba18662ad08
Aditya Barman <126593834+aditya-barman@users.noreply.github.com>
2023-12-18 15:02:45 +0530

    q11 done

--- q11_spearman_rank_corr.m
+++ q11_spearman_rank_corr.m
@@ -9,24 +9,33 @@ math_x = [43 77 64 96 48 35 86 71];
 phys_y = [36 68 49 79 50 41 82 65];
 n = length(math_x);
 
-R_x = [];
-R_y = [];
-
-d_sq = (R_x - R_y) .^ 2;
-d_sq_sum = 0;
+R_x = zeros(1, n);
+R_y = zeros(1, n);
 
 for i = 1:n
-    d_sq_sum = d_sq_sum + d_sq(i);
+    [valx, idx] = max(math_x);
+    math_x(idx) = -Inf;
+    R_x(idx) = i;
+    
+    [valy, idx] = max(phys_y);
+    phys_y(idx) = -Inf;
+    R_y(idx) = i;
 end
+
+d_sq = (R_x - R_y) .^ 2;
+d_sq_sum = sum(d_sq);
 
 sp_num = 6 * d_sq_sum;
 sp_denom = n * ((n ^ 2) - 1);
 
 sp = 1 - (sp_num / sp_denom);
 
-fprintf('');
-% ============== OUTPUT =============
+fprintf('The Spearman Rank Correlation is %.4f\n', sp);
 
-% 
 
-% ===================================
+
+% ================== OUTPUT =================
+
+% The Spearman Rank Correlation is 0.9286
+
+% ===========================================
