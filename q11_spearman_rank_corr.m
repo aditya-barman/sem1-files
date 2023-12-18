ef3a7d218acc482fd3a036537b292ea066e9b375
Aditya Barman <126593834+aditya-barman@users.noreply.github.com>
2023-12-18 14:27:29 +0530

    generated boilerplate for q11

--- q11_spearman_rank_corr.m
+++ q11_spearman_rank_corr.m
@@ -5,11 +5,28 @@
 
 clc, clearvars, close all
 
+math_x = [43 77 64 96 48 35 86 71];
+phys_y = [36 68 49 79 50 41 82 65];
+n = length(math_x);
 
+R_x = [];
+R_y = [];
 
+d_sq = (R_x - R_y) .^ 2;
+d_sq_sum = 0;
 
+for i = 1:n
+    d_sq_sum = d_sq_sum + d_sq(i);
+end
+
+sp_num = 6 * d_sq_sum;
+sp_denom = n * ((n ^ 2) - 1);
+
+sp = 1 - (sp_num / sp_denom);
+
+fprintf('');
 % ============== OUTPUT =============
 
-% Mean of the data is: 135.1556
+% 
 
 % ===================================
